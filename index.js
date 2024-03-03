993e8490764bbbfeab5cec988fa579d74dfb391c
eternity <114994799+iam-eternity@users.noreply.github.com>
2024-03-03 13:12:51 +0000

    ch

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const express = requestAnimationFrame
+const express = require('express')
+const app = express()
+const port = 3000
+require('dotenv').config()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter', (req, res) => {
+  res.send('Chai aur Twitter')
+})
+app.get('/youtube', (req, res) => {
+  res.send(`<h2>The name is youtube</h2>`)
+})
+app.get('/login', (req, res) => {
+  res.send(`<h1> Please Login </h1>`)
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
